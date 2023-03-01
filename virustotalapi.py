41f3e4acf06a8c71791220d906903ef07d38ea2f
Krrish <rockingdiv@gmail.com>
2023-03-01 13:17:34 +0530

    added the apifile

---
+++ virustotalapi.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+#print('Search files, URLs, domains, IPs and tag comments')
+x = input("Search files, URLs, domains, IPs and tag comments:\n")
+url = "https://www.virustotal.com/api/v3/search?query="+x
+
+headers = {
+    "accept": "application/json",
+    "x-apikey": "put your api key between the double quotes"
+}
+
+response = requests.get(url, headers=headers)
+data=response.json()
+#print(response.text)
+if len(data["data"]) > 0:
+    Malicious=("Malicious:", data["data"][0]["attributes"]["last_analysis_stats"]['malicious'])
+    Undetected=("Undetected:", data["data"][0]["attributes"]["last_analysis_stats"]['undetected'])
+    
+    if Malicious[-1] > 0:
+        print('\x1b[31m'+"🔴 MALICIOUS \n"+'\x1b[0m')
+        #print(Malicious,"\n",Undetected)
+        print("Malicious: ",Malicious[-1])
+        print("Undetected: ",Undetected[-1])
+    else:
+        print('\x1b[32m'+'🟢 CLEAN \n'+'\x1b[0m')
+        #print(Malicious,"\n",Undetected)
+        print("Malicious: ",Malicious[-1])
+        print("Undetected: ",Undetected[-1])
+else:
+    
+    print('\x1b[1;33;40m' +'\n🟡 NO DATA FOUND' + '\x1b[0m')
+
+'''    
+if Malicious[-1] > 0:
+    print('\x1b[31m'+"MALICIOUS \n"+'\x1b[0m')
+    #print(Malicious,"\n",Undetected)
+    print("Malicious: ",Malicious[-1])
+    print("Undetected: ",Undetected[-1])
+else:
+    print('\x1b[32m'+'CLEAN \n'+'\x1b[0m')
+    print(Malicious,"\n",Undetected)
+'''
+input("\n press ENTER to exit")
