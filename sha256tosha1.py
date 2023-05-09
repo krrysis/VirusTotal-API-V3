2fdb0aab2b2cdf9f444c4258cac94ef8b9f87bf1
krrysis <rockingdiv@gmail.com>
2023-05-09 11:39:36 +0530

    sha256tosha1 completed

--- sha256tosha1.py
+++ sha256tosha1.py
@@ -3,27 +3,35 @@ import json
 import re
 import csv
 
+#opens file with sha256 values
 with open('ioc.csv', newline='') as f:
     reader = csv.reader(f)
-    data = [item for sublist in reader for item in sublist]
+    ioclist = [item for sublist in reader for item in sublist]
+#blank output list which will be used to write to output csv file
+outputlist=[]
 
-for i in range(len(data)):
-    x=data[i]
+#loop to get sha1 from sha256 into a list
+for i in range(len(ioclist)):
+    x=ioclist[i]
     url = "https://www.virustotal.com/api/v3/search?query="+x
     headers = {
             "accept": "application/json",
-            "x-apikey": "your api key goes here"
+            "x-apikey": "1664211c7353df910d72ad121809dd94feb632ad198c361bfc53c6c640f8d540"
         }
     response = requests.get(url, headers=headers)
     data=response.json()
-
-    response = requests.get(url, headers=headers)
-    data=response.json()
+    
     pattern = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
     url_pattern = r"(https?://)?(www\.)?([a-zA-Z0-9]+(-?[a-zA-Z0-9])*\.)+[a-z]{2,}(/[\w\-]*)*"
     regex = re.compile(pattern)
     regexurl = re.compile(url_pattern)
-    
     if len(data["data"]) > 0:
         Sha1=("SHA1:", data["data"][0]["attributes"]["sha1"])
-        
+    outputlist.append(Sha1[-1])
+    #print(outputlist)
+    #print(data)
+
+#this will write outputlist to csv file
+with open('sha1.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows([[value] for value in outputlist])
